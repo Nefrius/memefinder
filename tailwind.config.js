5a0bc63dc270ed34030b7c008a88ee66520950cf
Nefrius <enesbas616161@gmail.com>
2024-08-03 19:35:04 +0300

    new ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './public/**/*.{html,js}',  // ya da dosyalarınızın bulunduğu uygun yol
+  ],
   theme: {
-    extend: {},
+
+    container: {
+      center: true,
+    },
+
+    extend: {  
+      fontFamily: {
+        gemunu: ['Montserrat', 'sans-serif'],
+      },
+
+      colors: {
+        'gega-yazi': '#ffff',
+        'gega-ic': '#393E46',
+        'gega-arkaplan': '#1E2022',
+        'gega-baslik': '#5C5470', 
+        // 4D4BCA 615EFC 
+      },
+    },
   },
-  plugins: [],
+  plugins: [],  
 }
+
 
